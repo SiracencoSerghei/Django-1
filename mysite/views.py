69f898e2b6ac03de41521e34c091655810cdfeab
Siracenco Serghei <siracencoserghei@gmail.com>
2025-11-11 21:49:42 +0100

    bootstrap implementing

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Welcome to the Home Page!")
+    # return HttpResponse("Welcome to the Home Page!")
+    # return HttpResponse(<h3>"Welcome to the Home Page!"</h3>)
+    return render(request, "home.html")
