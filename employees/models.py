0518da37e9a60717e0907e6c7011c39140136590
Siracenco Serghei <siracencoserghei@gmail.com>
2025-11-12 21:20:04 +0100

    migration

--- employees/models.py
+++ employees/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Employee(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    photo = models.ImageField(upload_to='employee_photos/')
+    photo = models.ImageField(upload_to="employee_photos/", blank=True)
     designation = models.CharField(max_length=50)
     email_address = models.EmailField(max_length=100, unique=True)
     phone_number = models.CharField(max_length=15, unique=True, blank=True)
